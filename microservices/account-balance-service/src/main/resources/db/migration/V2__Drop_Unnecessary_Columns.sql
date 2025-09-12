bcb399dd84cf6c8f88cd50a2362caeef842afc0b
Vishal Singh <vishal48@gmail.com>
2025-09-12 12:45:36 +0200

    with new globalexceptionHandler for all three services to throw proper error message in the response body.

--- microservices/account-balance-service/src/main/resources/db/migration/V2__Drop_Unnecessary_Columns.sql
+++
@@ -1,9 +0,0 @@
--- Check if the columns contain any data
-SELECT * FROM account_balance WHERE account_name IS NOT NULL OR account_type IS NOT NULL OR currency IS NOT NULL OR status IS NOT NULL;
-
--- If no data is found, proceed to drop the columns
-ALTER TABLE account_balance
-DROP COLUMN account_name,
-DROP COLUMN account_type,
-DROP COLUMN currency,
-DROP COLUMN status;
